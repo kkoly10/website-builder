ac545b14b6e8293402cbafacb3339a19566f448f
kkoly10 <komlankouhiko@icloud.com>
2026-02-13 23:00:41 -0500

    Update page.tsx

--- app/estimate/page.tsx
+++ app/estimate/page.tsx
@@ -1,13 +1,7 @@
 import EstimateClient from "./EstimateClient";
 
-type SearchParams = Record<string, string | string[] | undefined>;
-
 export const dynamic = "force-dynamic";
 
-export default function EstimatePage({
-  searchParams,
-}: {
-  searchParams: SearchParams;
-}) {
-  return <EstimateClient searchParams={searchParams} />;
+export default function EstimatePage() {
+  return <EstimateClient />;
 }
